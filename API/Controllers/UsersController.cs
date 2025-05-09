aa3d5cc5b5f3d9fff7a3a16c439204f87f9acce9
David-Campos-maker <davidcamposcruz2016@gmail.com>
2025-05-09 14:14:51 -0300

    Update member

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -14,14 +15,11 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly IMapper _mapper = mapper;
 
-        public UsersController(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
-        }
 
         [HttpGet]
         public async Task<IActionResult> GetUsers()
@@ -39,6 +37,22 @@ namespace API.Controllers
             if (user == null) return NotFound();
 
             return Ok(user);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(MemberUpdateDto memberDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (username == null) return BadRequest("No username found in token");
+
+            var user = await _userRepository.GetByUsernameAsync(username);
+            if (user == null) return BadRequest("User not found");
+
+            _mapper.Map(memberDto, user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
     }
 }
