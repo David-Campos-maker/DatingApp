6f9ea4cfdc70bfbc76b549787b7d4ceda1ef45ea
David-Campos-maker <davidcamposcruz2016@gmail.com>
2025-05-08 23:02:26 -0300

    UI interaction

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027f5b4575935f30618b5489b6a957d73982fc7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca6cc7d0212e668d3e9666d8cad122e5d43c3e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import Member from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
@@ -11,11 +12,13 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.scss'
 })
-export class MemberEditComponent implements OnInit{
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
   member?: Member;
 
   private readonly accountService = inject(AccountService);
   private readonly memberService = inject(MembersService);
+  private readonly toastr = inject(ToastrService);
 
 
   ngOnInit(): void {
@@ -28,5 +31,11 @@ export class MemberEditComponent implements OnInit{
     this.memberService.getMember(user.username).subscribe({
       next: member => this.member = member
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
